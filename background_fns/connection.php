d66d9f94404bd35714d3f040be5fbc718d4a0a24
Thomas Penny <66413737+thomaspenny@users.noreply.github.com>
2025-11-21 12:16:21 +0000

    Obscure database connection credentials
    Replaced sensitive database credentials with placeholders.

--- background_fns/connection.php
+++ background_fns/connection.php
@@ -1,9 +1,9 @@
 <?php
 // variables to hold database login info
-$dbhost = 'fdb1034.awardspace.net';
-$dbuser = '4707790_palpostthomaspenny';
-$dbpass = 'annabellapenny94';
-$dbname = '4707790_palpostthomaspenny';
+$dbhost = '<>';
+$dbuser = '<>';
+$dbpass = '<>';
+$dbname = '<>';
 
 //Attempt to connect to database
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
